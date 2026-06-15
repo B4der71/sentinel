b0007ee6d486ad42a328d7d8fb9b82c5e30e2d19
B4der71 <baderwashah111@gmail.com>
2026-06-15 00:59:23 -0400

    Update version

--- src/sentinel/__init__.py
+++ src/sentinel/__init__.py
@@ -4,4 +4,4 @@ For AUTHORISED testing only. The scope layer enforces a default-deny host
 allow-list; you are responsible for ensuring you have permission to test any
 target you place in scope.
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
